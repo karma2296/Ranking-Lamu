d483955e137fc9123c92d519b0ae0c60f102654c
karma2296 <128663883+karma2296@users.noreply.github.com>
2026-01-01 11:43:23 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   define: {
-    // Esto inyecta las variables en el código del navegador durante la compilación
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY || ''),
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
   },
